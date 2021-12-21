0c5fe6787625743aa7f8e4affd135b21d6ef53f3
pavanbhat1999 <prbhat07@gmail.com>
2021-12-21 21:33:00 +0530

    TEsted and stable

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 vim.cmd[[autocmd VimEnter * :silent exec "!kill -s SIGWINCH $PPID"]]
--- vim.cmd('source $HOME/.config/nvim/vim-scripts/plugins.vim')--TODO:Think about changing package manager
 require("plugins")
 require('defaults')
 require("color-settings")
