d0a342c69cd3f9cffafb090c7d4b65b455e9e82f
pavanbhat1999 <prbhat07@gmail.com>
2021-12-22 22:17:08 +0530

    2nd part in plugin

--- lua/firstPlugin/init.lua
+++ lua/firstPlugin/init.lua
@@ -1,9 +1,16 @@
-local function printW()
-  print("Hello World!")
+local function createFloatingWindow()
   local width = vim.api.nvim_win_get_width(0)
   print(width)
+  local bufh = vim.api.nvim_create_buf(false,true)
+  local winId = vim.api.nvim_open_win(bufh,true,{
+	  relative='editor',
+	  width=width-4,
+	  height=10,
+	  col=2,
+	  row=2,
+  })
 
 end
 return {
-	    printW = printW
+	    createFloatingWindow = createFloatingWindow
 }
