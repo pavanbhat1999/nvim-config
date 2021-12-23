dddffc01f52336a8ea01b59d89ba396e4643c61a
pavanbhat1999 <prbhat07@gmail.com>
2021-12-23 17:22:44 +0530

    chane

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ vim.cmd [[autocmd VimEnter * :silent exec "!kill -s SIGWINCH $PPID"]]
 require("plugins")
 require('defaults')
 require("color-settings")
-require("key-mappings") -- TODO:Convert all mappings to lua standard
+require("key-mappings") -- TODO:Convert all mappings to lua standard--------------------------------
 require("cmp-config")
 require('lsp/python-lsp')
 require("lsp/bash-lsp")
@@ -14,5 +14,5 @@ require("harpoon-settings")
 require("treesitter")
 require("efm")
 -- require("null-ls-config")
--- Need port of auto command from neovim
+-- Need port of auto command from neovim------------------------------------------------------------
 vim.cmd('source $HOME/.config/nvim/vim-scripts/auto-cmd.vim')
